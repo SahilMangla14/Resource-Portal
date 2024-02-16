aaf47e0315c9c25b0b0acced065c2df9d9586283
Ojassvi Kumar <kumarojassvi@gmail.com>
2024-02-17 05:19:11 +0530

    Minor update in home page design

--- Frontend/app/temp-home/page.tsx
+++ Frontend/app/temp-home/page.tsx
@@ -29,14 +29,14 @@ export default function Page() {
                         enableSystem
                         disableTransitionOnChange
                     >
-                        
+
                         <NavigationBar />
                         <div className="flex h-[600px]">
                             <div className="w-[50%] flex items-center px-16">
-                                <div>
-                                <div className="px-10 text-xl">Welcome to,</div>
-                                <div className="px-12 text-8xl font-bold">InfoNest</div>
-                                <div className="px-12 text-muted-foreground text-md">Your one stop destination for everything.</div>
+                                <div className="border rounded-lg py-12 pr-24 dark:bg-slate-950">
+                                    <div className="px-10 text-xl">Welcome to,</div>
+                                    <div className="px-12 text-8xl font-bold">InfoNest</div>
+                                    <div className="px-12 text-muted-foreground text-md">Your one stop destination for everything.</div>
                                 </div>
                             </div>
                             <div className="w-[50%] flex justify-center items-center">
